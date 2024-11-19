74245ffb11a8c7c0662f08e27597bcd4be3de08d
Steven Marcel Bißantz <steven.bissantz@gmail.com>
2024-11-19 09:44:59 +0100

    Update 04d_Observed_Polytomous_Data.R

--- 04_Psychometric_Modeling/04d_Observed_Polytomous_Data.R
+++ 04_Psychometric_Modeling/04d_Observed_Polytomous_Data.R
@@ -577,14 +577,12 @@ hist(modelBinomial_samples$summary(variables = c("theta"))$sd-
 # aka. Nominal Response Model – in IRT     #
 ############################################
 
-# todo todo todo
-
-conspiracyItems = citems
-
 # Compile model
 mdl_2pclsi <- cmdstan_model("./stan/4d/2pcl_si.stan", pedantic = TRUE)
 
-# item threshold hyperparameters
+# Hyperparameters
+
+# Item threshold hyperparameters
 mu_mean <- rep(0, I)
 Mu_mean <- replicate(C - 1, mu_mean) 
 
@@ -594,6 +592,7 @@ for(d in seq_len(I)) {
   MU_cov[d , ,] <- diag(1, C - 1)
 }
 
+# Discrimination hyperparameters
 lambda_mean <- rep(0, I)
 Lambda_mean <- replicate(C - 1, mu_mean) 
 
@@ -603,8 +602,11 @@ for(d in seq_len(I)) {
   LAMBDA_cov[d , ,] <- diag(1, C - 1)
 }
 
-# todo todo todo: Why?
+# Standardized sum scores to initialize LV
+sum_scores <- rowSums(citems)
+theta_init <- (sum_scores - mean(sum_scores)) / sd(sum_scores)
 
+# Stan list
 stanls_2pclsi <- list(
   "P" = P,
   "I" = I,
@@ -616,11 +618,7 @@ stanls_2pclsi <- list(
   "Lambda_cov" = LAMBDA_cov
 )
 
-# Standardized sum scores to initialize LV
-sum_scores <- rowSums(citems)
-theta_init <- (sum_scores - mean(sum_scores)) / sd(sum_scores)
-
-# Fit the model to the data 
+# Check initial values
 fit_2pclsi <- mdl_2pclsi$sample(
   data = stanls_2pclsi,
   seed = 112,
@@ -628,114 +626,28 @@ fit_2pclsi <- mdl_2pclsi$sample(
   parallel_chains = 4,
   iter_warmup = 0,
   iter_sampling = 5,
-  # Important: We do not initialize lambda, because lambda can be negative in this model. Every category gets its own lambda, so if we set them all to by very positve that would not soak up in the right mode / in the right spot.
-  init = function() list(theta = rnorm(P, mean = theta_init, sd = 0)),
+  init = function() list("theta" = rnorm(P, mean = theta_init, sd = 0)),
   adapt_engaged = FALSE
 )
+fit_2pclsi$draws(variables = "lambda_init")
 
-# todo todo todo: Model does not run
+# todo todo : What is adapt_engaged?
 
-
-modelCategoricalLogit_samples$draws(variables = "initLambda")
-
-modelCategoricalLogit_samples = modelCategoricalLogit_stan$sample(
-  data = modelOrderedLogit_data,
-  seed = 121120222,
+fit_2pclsi <- mdl_2pclsi$sample(
+  data = stanls_2pclsi,
+  seed = 112,
   chains = 4,
   parallel_chains = 4,
-  iter_warmup = 1000,
-  iter_sampling = 1000,
+  iter_warmup = 3000,
+  iter_sampling = 2000,
   #init = function() list(initLambda=rnorm(nItems*(maxCategory-1), mean=1, sd=.1))
-  init = function() list(theta=rnorm(nObs, mean=thetaInit, sd=0))
+  # Important: We do not initialize lambda, because lambda can be negative in this model. Every category gets its own lambda, so if we set them all to by very positve that would not soak up in the right mode / in the right spot.
+  init = function() list("theta" = rnorm(P, mean = theta_init, sd = 0))
 )
 
+# todo todo
 
-# checking convergence
-max(modelCategoricalLogit_samples$summary()$rhat, na.rm = TRUE)
-
-print(modelCategoricalLogit_samples$summary(variables = c("mu", "lambda")), n=Inf)
-
-11 mu[1,2]   1.08    1.07   0.288 0.290  0.613   1.55    1.00    1520.    2106.
- 12 mu[2,2]   0.373   0.367  0.249 0.245 -0.0402  0.788   1.00    2144.    2668.
- 13 mu[3,2]  -0.497  -0.495  0.271 0.266 -0.944  -0.0488  1.00    2134.    2668.
- 14 mu[4,2]   0.559   0.559  0.278 0.287  0.111   1.03    1.00    1824.    2785.
- 15 mu[5,2]   0.649   0.639  0.269 0.257  0.215   1.10    1.00    1657.    2300.
- 16 mu[6,2]   0.639   0.637  0.282 0.277  0.178   1.12    1.00    1854.    2789.
- 17 mu[7,2]  -0.310  -0.311  0.241 0.242 -0.698   0.0860  1.00    2237.    2585.
- 18 mu[8,2]   0.338   0.335  0.269 0.280 -0.0939  0.786   1.00    1807.    2661.
- 19 mu[9,2]  -0.339  -0.338  0.234 0.231 -0.728   0.0444  1.00    2129.    2745.
- 20 mu[10,2] -1.46   -1.46   0.260 0.265 -1.90   -1.06    1.00    4217.    2979.
- 21 mu[1,3]   0.949   0.943  0.305 0.305  0.455   1.46    1.00    1440.    2298.
- 22 mu[2,3]  -0.734  -0.734  0.371 0.371 -1.35   -0.139   1.00    1535.    2439.
- 23 mu[3,3]  -0.216  -0.217  0.260 0.265 -0.637   0.203   1.00    1803.    2711.
- 24 mu[4,3]   0.146   0.154  0.337 0.334 -0.417   0.685   1.00    1617.    2786.
- 25 mu[5,3]  -0.619  -0.601  0.412 0.406 -1.33    0.0341  1.00    1342.    2192.
- 26 mu[6,3]  -0.0562 -0.0546 0.368 0.369 -0.685   0.544   1.00    1689.    2484.
- 27 mu[7,3]  -1.16   -1.16   0.345 0.343 -1.75   -0.607   1.00    2688.    2463.
- 28 mu[8,3]  -0.196  -0.192  0.350 0.350 -0.788   0.353   1.00    1420.    2507.
- 29 mu[9,3]  -1.72   -1.70   0.410 0.410 -2.41   -1.08    1.00    1898.    2414.
- 30 mu[10,3] -2.39   -2.37   0.393 0.395 -3.07   -1.78    1.00    3164.    2694.
- 31 mu[1,4]   0.313   0.317  0.323 0.328 -0.225   0.836   1.00    1670.    2397.
- 32 mu[2,4]  -1.13   -1.12   0.359 0.361 -1.73   -0.539   1.00    2686.    3013.
- 33 mu[3,4]  -1.90   -1.88   0.409 0.404 -2.61   -1.26    1.00    3633.    2639.
- 34 mu[4,4]  -1.31   -1.29   0.419 0.417 -2.02   -0.635   1.00    3379.    2848.
- 35 mu[5,4]  -0.758  -0.749  0.378 0.382 -1.39   -0.146   1.00    2122.    2790.
- 36 mu[6,4]  -1.11   -1.09   0.416 0.404 -1.81   -0.439   1.00    2767.    2661.
- 37 mu[7,4]  -1.93   -1.91   0.392 0.380 -2.59   -1.31    1.00    3980.    3192.
- 38 mu[8,4]  -1.76   -1.74   0.456 0.454 -2.54   -1.05    1.00    3348.    3064.
- 39 mu[9,4]  -1.41   -1.40   0.328 0.322 -1.97   -0.885   1.00    3079.    2941.
- 40 mu[10,4] -3.25   -3.22   0.480 0.487 -4.06   -2.50    1.00    4821.    2922.
- 41 mu[1,5]  -0.975  -0.962  0.444 0.441 -1.71   -0.264   1.00    2396.    2969.
- 42 mu[2,5]  -1.50   -1.48   0.398 0.394 -2.17   -0.872   1.00    3462.    2963.
- 43 mu[3,5]  -1.97   -1.95   0.419 0.404 -2.70   -1.31    1.00    3781.    2957.
- 44 mu[4,5]  -1.06   -1.05   0.386 0.385 -1.70   -0.434   1.00    3326.    3364.
- 45 mu[5,5]  -1.39   -1.38   0.430 0.410 -2.13   -0.710   1.00    3043.    2806.
- 46 mu[6,5]  -1.77   -1.75   0.488 0.484 -2.59   -0.999   1.00    3538.    2849.
- 47 mu[7,5]  -2.29   -2.27   0.433 0.434 -3.03   -1.61    1.00    4175.    2684.
- 48 mu[8,5]  -2.13   -2.11   0.503 0.509 -2.98   -1.34    1.00    4038.    3292.
- 49 mu[9,5]  -1.91   -1.89   0.370 0.365 -2.52   -1.33    1.00    3361.    3098.
- 50 mu[10,5] -2.21   -2.20   0.339 0.338 -2.79   -1.69    1.00    3877.    2339.
- 61 lambda[…  1.20    1.19   0.236 0.228  0.827   1.61    1.00    1551.    2444.
- 62 lambda[…  1.33    1.32   0.255 0.247  0.933   1.77    1.00    2392.    2556.
- 63 lambda[…  1.59    1.58   0.289 0.288  1.12    2.08    1.00    2685.    2867.
- 64 lambda[…  1.75    1.74   0.284 0.278  1.28    2.23    1.00    2389.    3093.
- 65 lambda[…  1.60    1.59   0.283 0.279  1.15    2.09    1.00    2191.    2379.
- 66 lambda[…  2.04    2.03   0.302 0.298  1.55    2.54    1.00    2429.    2694.
- 67 lambda[…  1.44    1.42   0.269 0.261  1.02    1.91    1.00    2917.    2644.
- 68 lambda[…  1.70    1.69   0.301 0.294  1.23    2.22    1.00    2471.    2824.
- 69 lambda[…  1.37    1.36   0.256 0.253  0.972   1.81    1.00    2866.    2830.
- 70 lambda[…  1.26    1.24   0.285 0.281  0.807   1.74    1.00    3078.    2793.
- 71 lambda[…  1.92    1.91   0.281 0.284  1.47    2.39    1.00    1765.    2819.
- 72 lambda[…  2.78    2.75   0.413 0.407  2.14    3.49    1.00    2449.    2732.
- 73 lambda[…  1.79    1.79   0.294 0.297  1.33    2.30    1.00    2334.    2615.
- 74 lambda[…  2.79    2.78   0.381 0.377  2.18    3.43    1.00    2266.    2891.
- 75 lambda[…  3.35    3.33   0.451 0.442  2.63    4.12    1.00    2544.    2613.
- 76 lambda[…  3.11    3.10   0.413 0.404  2.46    3.81    1.00    2481.    2598.
- 77 lambda[…  2.28    2.26   0.374 0.366  1.69    2.93    1.00    2565.    2633.
- 78 lambda[…  3.10    3.07   0.448 0.440  2.41    3.89    1.00    2411.    2789.
- 79 lambda[…  2.55    2.54   0.403 0.409  1.89    3.22    1.00    2526.    2590.
- 80 lambda[…  1.84    1.83   0.382 0.364  1.25    2.50    1.00    2856.    2961.
- 81 lambda[…  1.46    1.46   0.289 0.289  1.00    1.94    1.00    1945.    2759.
- 82 lambda[…  1.86    1.85   0.402 0.393  1.22    2.53    1.00    3276.    2749.
- 83 lambda[…  1.59    1.58   0.414 0.412  0.914   2.29    1.00    3039.    2778.
- 84 lambda[…  1.89    1.88   0.460 0.457  1.16    2.68    1.00    3797.    3113.
- 85 lambda[…  2.27    2.25   0.425 0.426  1.59    2.99    1.00    3073.    3060.
- 86 lambda[…  2.31    2.30   0.467 0.459  1.55    3.07    1.00    2731.    3069.
- 87 lambda[…  1.63    1.63   0.425 0.427  0.932   2.35    1.00    3436.    3030.
- 88 lambda[…  1.95    1.93   0.539 0.547  1.09    2.86    1.00    3080.    2322.
- 89 lambda[…  1.56    1.54   0.361 0.354  0.986   2.16    1.00    3330.    2759.
- 90 lambda[…  0.863   0.861  0.460 0.467  0.113   1.62    1.00    4392.    2698.
- 91 lambda[…  2.07    2.06   0.429 0.423  1.39    2.81    1.00    2149.    2732.
- 92 lambda[…  1.79    1.78   0.454 0.462  1.06    2.54    1.00    2544.    2430.
- 93 lambda[…  1.77    1.76   0.438 0.446  1.06    2.50    1.00    2557.    2521.
- 94 lambda[…  1.76    1.74   0.437 0.437  1.05    2.50    1.00    2873.    2917.
- 95 lambda[…  2.11    2.09   0.521 0.525  1.26    2.99    1.00    2503.    2510.
- 96 lambda[…  2.07    2.07   0.544 0.534  1.19    2.97    1.00    2917.    2837.
- 97 lambda[…  1.56    1.54   0.455 0.455  0.830   2.33    1.00    2988.    2711.
- 98 lambda[…  1.61    1.59   0.550 0.543  0.731   2.54    1.00    3735.    2802.
- 99 lambda[…  1.49    1.47   0.421 0.413  0.800   2.21    1.00    2775.    2692.
-100 lambda[…  1.27    1.25   0.363 0.367  0.697   1.87    1.00    2085.    2299.
-
+fit_2pclsi$summary(variables = "mu_init")
 
 
 ## investigating option characteristic curves ===================================
