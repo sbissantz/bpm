e2dbee4ff447d9d223350eaf169c9f971bd29381
Steven Marcel Bißantz <steven.bissantz@gmail.com>
2024-12-20 11:30:13 +0100

    Update 04h_Scale_identification.R

--- 04_Psychometric_Modeling/04h_Scale_identification.R
+++ 04_Psychometric_Modeling/04h_Scale_identification.R
@@ -30,6 +30,11 @@ P <- nrow(citems)
 # Note: Successive integers from 1 to highest 
 C <- max(citems)
 
+# Number of dimensions
+D <- 2
+# Test if the data are conformable with two factors: 
+# (1) A government factor & (2) a non-goverment factor 
+
 # Item means
 # Note: the mean is the proportion of respondents who agreed with the item
 colMeans(citems)
@@ -236,232 +241,107 @@ plot(sd(draws_2polsim$lambda), sd(draws_2polsi$lambda),
      xlab = "Marker Item", ylab = "Standardized Factor", main = "Comparing Theta EAD Estimates")
 # The marker item shifts where the following lambda values go
 
-# todo todo todo!
-
-# Multidimesnional GRM + Factor Variance: Strong identification (model/data likelihood and posterior) ===========================
-
-# todo todo
+###########################################
+# Multidimensional 2POL with marker items #
+# MD-GRM                                  #
+###########################################
 
 # Compile model
 mdl_md2polsim <- cmdstan_model("./stan/4h/md2pol_si_marker.stan",
   pedantic = TRUE)
 
-# Check code again!
+############
+# Q-Matrix #
+############
 
-modelMultidimensionalGRM_markerItem_syntax = "
-data {
-  
-  // data specifications  =============================================================
-  int<lower=0> nObs;                            // number of observations
-  int<lower=0> nItems;                          // number of items
-  int<lower=0> maxCategory;                     // number of categories for each item
-  
-  // input data  =============================================================
-  array[nItems, nObs] int<lower=1, upper=5>  Y; // item responses in an array
+# Initialize a I X D matrix with zeros
+Q <- matrix(0, I, D)
+colnames(Q) <- c("Gov", "NonGov")
+rownames(Q) <- paste0("i", seq_len(I))
+# Determine which items measure each factor
+# Factor 1 is measured by (loads on) items...
+itms_d1 <- c(2, 5, 7, 8, 9)
+Q[itms_d1, 1] <- 1
+# Factor 2 is measured by (loads on) items...
+itms_d2 <- c(1, 3, 4, 6, 10)
+Q[itms_d2, 2] <- 1
 
-  // loading specifications  =============================================================
-  int<lower=1> nFactors;                                       // number of loadings in the model
-  array[nItems, nFactors] int<lower=0, upper=1> Qmatrix;
-  
-  // prior specifications =============================================================
-  array[nItems] vector[maxCategory-1] meanThr;                // prior mean vector for intercept parameters
-  array[nItems] matrix[maxCategory-1, maxCategory-1] covThr;  // prior covariance matrix for intercept parameters
-  
-  vector[nItems-nFactors] meanLambda;         // prior mean vector for discrimination parameters
-  matrix[nItems-nFactors, nItems-nFactors] covLambda;  // prior covariance matrix for discrimination parameters
-  
-  vector[nFactors] meanTheta; 
-  vector[nFactors] sdThetaLocation;
-  vector[nFactors] sdThetaScale;
+# Import: We have no multidimensional items, every factor is measured by a
+# unique set of items.
+Q
+
+# Item threshold hyperparameter
+Thr_mean <- replicate(C - 1, rep(0, I)) # 10 x 4
+THR_cov <- array(0, dim = c(10, 4, 4)) # 10 x 4 x 4
+for(d in seq_len(I)) {
+  THR_cov[d , ,] <- diag(10, C - 1)
 }
 
-transformed data{
-  int<lower=0> nLoadings = 0;                                      // number of loadings in model
-  array[nFactors] int<lower=0> markerItem = rep_array(0, nFactors);
-  
-  for (factor in 1:nFactors){
-    nLoadings = nLoadings + sum(Qmatrix[1:nItems, factor]);
-  }
+# Item discrimination/factor loading hyperparameters
+# Note: I - D because of marker items
+lambda_mean <- rep(0, I - D)
+Lambda_cov <- diag(10, I - D)
 
-  array[nLoadings, 4] int loadingLocation;                     // the row/column positions of each loading, plus marker switch
-  
-  int loadingNum=1;
-  int lambdaNum=1;
-  for (item in 1:nItems){
-    for (factor in 1:nFactors){       
-      if (Qmatrix[item, factor] == 1){
-        loadingLocation[loadingNum, 1] = item;
-        loadingLocation[loadingNum, 2] = factor;
-        if (markerItem[factor] == 0){
-          loadingLocation[loadingNum, 3] = 1;     // ==1 if marker item, ==0 otherwise
-          loadingLocation[loadingNum, 4] = 0;     // ==0 if not one of estimated lambdas
-          markerItem[factor] = item;
-        } else {
-          loadingLocation[loadingNum, 3] = 0;
-          loadingLocation[loadingNum, 4] = lambdaNum;
-          lambdaNum = lambdaNum + 1;
-        }
-        loadingNum = loadingNum + 1;
-      }
-    }
-  }
+# Latent trait hyperparameters
+Theta_mean <- rep(0, D)
+theta_sd_loc <- rep(0, D)
+theta_sd_scl <- rep(0.5, D)
 
+#############
+# Stan list #
+#############
 
-}
-
-parameters {
-  array[nObs] vector[nFactors] theta;                // the latent variables (one for each person)
-  array[nItems] ordered[maxCategory-1] thr; // the item thresholds (one for each item category minus one)
-  vector[nLoadings-nFactors] initLambda;             // the factor loadings/item discriminations (one for each item)
-  
-  cholesky_factor_corr[nFactors] thetaCorrL;
-  vector<lower=0>[nFactors] thetaSD;
-}
-
-transformed parameters{
-  matrix[nItems, nFactors] lambdaMatrix = rep_matrix(0.0, nItems, nFactors);
-  matrix[nObs, nFactors] thetaMatrix;
-  
-  // build matrix for lambdas to multiply theta matrix
-  
-  for (loading in 1:nLoadings){  
-    if (loadingLocation[loading,3] == 1){
-      lambdaMatrix[loadingLocation[loading,1], loadingLocation[loading,2]] = 1.0;
-    } else {
-      lambdaMatrix[loadingLocation[loading,1], loadingLocation[loading,2]] = initLambda[loadingLocation[loading,4]];
-    }
-  }
-  
-  for (factor in 1:nFactors){
-    thetaMatrix[,factor] = to_vector(theta[,factor]);
-  }
-  
-}
-
-model {
-  
-  matrix[nFactors, nFactors] thetaCovL;
-  initLambda ~ multi_normal(meanLambda, covLambda); 
-  
-  thetaCorrL ~ lkj_corr_cholesky(1.0);
-  thetaSD ~ lognormal(sdThetaLocation,sdThetaScale);
-  
-  thetaCovL = diag_pre_multiply(thetaSD, thetaCorrL);
-  theta ~ multi_normal_cholesky(meanTheta, thetaCovL);    
-  
-  
-  for (item in 1:nItems){
-    thr[item] ~ multi_normal(meanThr[item], covThr[item]);            
-    Y[item] ~ ordered_logistic(thetaMatrix*lambdaMatrix[item,1:nFactors]', thr[item]);
-  }
-  
-  
-}
-
-generated quantities{ 
-  array[nItems] vector[maxCategory-1] mu;
-  corr_matrix[nFactors] thetaCorr;
-  cholesky_factor_cov[nFactors] thetaCov_pre;
-  cov_matrix[nFactors] thetaCov; 
-  
-  for (item in 1:nItems){
-    mu[item] = -1*thr[item];
-  }
-  
-  thetaCorr = multiply_lower_tri_self_transpose(thetaCorrL);
-  thetaCov_pre = diag_pre_multiply(thetaSD, thetaCorrL);
-  thetaCov = multiply_lower_tri_self_transpose(thetaCov_pre);
-}
-"
-
-modelMultidimensionalGRM_markerItem_stan = cmdstan_model(stan_file = write_stan_file(modelMultidimensionalGRM_markerItem_syntax))
-
-
-# Build a Q-Matrix ===========================================================================
-
-Qmatrix = matrix(data = 0, nrow = ncol(conspiracyItems), ncol = 2)
-colnames(Qmatrix) = c("Gov", "NonGov")
-rownames(Qmatrix) = paste0("item", 1:ncol(conspiracyItems))
-Qmatrix[1,2] = 1
-Qmatrix[2,1] = 1
-Qmatrix[3,2] = 1
-Qmatrix[4,2] = 1
-Qmatrix[5,1] = 1
-Qmatrix[6,2] = 1
-Qmatrix[7,1] = 1
-Qmatrix[8,1] = 1
-Qmatrix[9,1] = 1
-Qmatrix[10,2] = 1
-
-Qmatrix
-
-
-# Data needs: successive integers from 1 to highest number (recode if not consistent)
-maxCategory = 5
-
-# data dimensions
-nObs = nrow(conspiracyItems)
-nItems = ncol(conspiracyItems)
-nFactors = ncol(Qmatrix)
-
-# item threshold hyperparameters
-thrMeanHyperParameter = 0
-thrMeanVecHP = rep(thrMeanHyperParameter, maxCategory-1)
-thrMeanMatrix = NULL
-for (item in 1:nItems){
-  thrMeanMatrix = rbind(thrMeanMatrix, thrMeanVecHP)
-}
-
-thrVarianceHyperParameter = 1000
-thrCovarianceMatrixHP = diag(x = thrVarianceHyperParameter, nrow = maxCategory-1)
-thrCovArray = array(data = 0, dim = c(nItems, maxCategory-1, maxCategory-1))
-for (item in 1:nItems){
-  thrCovArray[item, , ] = diag(x = thrVarianceHyperParameter, nrow = maxCategory-1)
-}
-
-# item discrimination/factor loading hyperparameters
-lambdaMeanHyperParameter = 0
-lambdaMeanVecHP = rep(lambdaMeanHyperParameter, nItems-nFactors)
-
-lambdaVarianceHyperParameter = 1000
-lambdaCovarianceMatrixHP = diag(x = lambdaVarianceHyperParameter, nrow = nItems-nFactors)
-
-# theta hyperparameters
-thetaMean = rep(0, nFactors)
-sdThetaLocation = rep(0, nFactors)
-sdThetaScale = rep(.5, nFactors)
-
-modelMultidimensionalGRM_markerItem_data = list(
-  nObs = nObs,
-  nItems = nItems,
-  maxCategory = maxCategory,
-  Y = t(conspiracyItems), 
-  nFactors = nFactors,
-  Qmatrix = Qmatrix,
-  meanThr = thrMeanMatrix,
-  covThr = thrCovArray,
-  meanLambda = lambdaMeanVecHP,
-  covLambda = lambdaCovarianceMatrixHP,
-  meanTheta = thetaMean,
-  sdThetaLocation = sdThetaLocation,
-  sdThetaScale = sdThetaScale
+stanls_md2polsim <- list(
+  "P" = P,
+  "I" = I,
+  "C" = C,
+  "D" = D,
+  "Q" = Q,
+  # Important transpose (array in stan are in row major order)
+  "Y" = t(citems),
+  "thr_mean" = Thr_mean,
+  "Thr_cov" = THR_cov,
+  "lambda_mean" = lambda_mean,
+  "Lambda_cov" = Lambda_cov,
+  "theta_mean" = Theta_mean,
+  "theta_sd_loc" = theta_sd_loc,
+  "theta_sd_scl" = theta_sd_scl
 )
 
-modelMultidimensionalGRM_markerItem_samples = modelMultidimensionalGRM_markerItem_stan$sample(
-  data = modelMultidimensionalGRM_markerItem_data,
-  seed = 201120224,
+# Multiple standardized sum scores to start theta in the correct location
+lambda_init <- rnorm(I, mean = 5, sd = 1)
+sum_scores <- as.matrix(citems) %*% Q
+
+# Standardize sum scores (mean: 0, sd: 1)
+theta_init <- scale(sum_scores)
+
+# Set any missing values to a random number
+na_pat <- which(is.na(thetaInit))
+theta_init[na_pat] <- rnorm(1, 0, 1)
+
+# Run MCMC chain (sample from posterior p.d.)
+fit_md2polsim <- mdl_md2polsim$sample(
+  data = stanls_md2polsim,
+  seed = 112,
   chains = 4,
   parallel_chains = 4,
-  iter_warmup = 2000,
+  iter_warmup = 3000,
   iter_sampling = 2000,
-  thin = 2,
-  init = function() list(lambda=rnorm(nItems, mean=10, sd=1))
+  # Mean should be below 10, since the log of it is too large
+  init = function() list("lambda" = lambda_init, "theta" = theta_init)
 )
 
- # checking convergence
-max(modelMultidimensionalGRM_markerItem_samples$summary()$rhat, na.rm = TRUE)
+##############
+# Diagnostic #
+##############
+
+# Checking convergence
+fit_md2polsim$cmdstan_diagnose()
+fit_md2polsim$diagnostic_summary()
+max(fit_md2polsim$summary()$rhat, na.rm = TRUE)
 
 # item parameter results
-print(modelMultidimensionalGRM_markerItem_samples$summary(variables = c("thetaSD", "thetaCov", "thetaCorr", "lambdaMatrix", "mu")) ,n=Inf)
-
-save.image(file = "lecture04h.RData")
+print(
+  fit_md2polsim$summary(
+    variables = c("theta_sd", "Theta_cov", "Theta_cor", "Lambda", "mu")
+  ), n = Inf)
